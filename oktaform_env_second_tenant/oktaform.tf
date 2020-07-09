2a633cd21ce9d1487542cddcb89832c457bd61d2
Andrew Van Beek <andrew.vanbeek@okta.com>
2020-07-09 00:00:18 -0500

    auth server children working

--- oktaform_env_second_tenant/oktaform.tf
+++ oktaform_env_second_tenant/oktaform.tf
@@ -1,20 +1,41 @@
-resource "okta_policy_signon" "newserverausl6l5880dElOP840h7" {
+resource "okta_auth_server_claim" "testoneoclskp6egedfXZ38x0h7" {
 status = "ACTIVE"
-name = "new server"
-description = "blah blah"
+name = "test_one"
+value_type = "EXPRESSION"
+value = "user.firstName"
+claim_type = "RESOURCE"
+auth_server_id = "${okta_auth_server.keycloakauslrka4f79eAysUq0h7.id}"
 }
-resource "okta_auth_server" "defaultausgub6xfsZCb1A5O0h7" {
-audiences = ["api://payments"]
-description = "Default Authorization Server for your Applications"
-name = "default"
-}
-resource "okta_policy_signon" "defaultausgub6xfsZCb1A5O0h7" {
+resource "okta_auth_server_claim" "thisiscustomoclskp80r4plYVlkx0h7" {
 status = "ACTIVE"
-name = "default"
-description = "Default Authorization Server for your Applications"
+name = "this_is_custom"
+value_type = "EXPRESSION"
+value = "\"custom\""
+claim_type = "RESOURCE"
+auth_server_id = "${okta_auth_server.keycloakauslrka4f79eAysUq0h7.id}"
 }
-resource "okta_auth_server" "newserverausl6l5880dElOP840h7" {
-audiences = ["http://localhost:3000"]
-description = "blah blah"
-name = "new server"
+resource "okta_auth_server_policy" "Forall00plrkivr3fJyiLhQ0h7" {
+status = "ACTIVE"
+name = "For all"
+description = "for all"
+priority = 1
+client_whitelist = ["ALL_CLIENTS"]
+auth_server_id = "${okta_auth_server.keycloakauslrka4f79eAysUq0h7.id}"
+}
+resource "okta_auth_server_scope" "lolscpskp64i5ONKHIHw0h7" {
+consent = "IMPLICIT"
+name = "lol"
+description = "lol"
+auth_server_id = "${okta_auth_server.keycloakauslrka4f79eAysUq0h7.id}"
+}
+resource "okta_auth_server_scope" "spascpskp9bwjpd42gLv0h7" {
+consent = "IMPLICIT"
+name = "spa"
+description = "spa"
+auth_server_id = "${okta_auth_server.keycloakauslrka4f79eAysUq0h7.id}"
+}
+resource "okta_auth_server" "keycloakauslrka4f79eAysUq0h7" {
+audiences = ["http://localhost:8180"]
+description = null
+name = "keycloak"
 }
